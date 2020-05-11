ac66ddfca319dfd29eb4fbb94c08326f6aad7643
Manuel Sánchez Velázquez <49983557+manusanchev@users.noreply.github.com>
2020-05-11 20:53:38 +0200

    cambios diseño

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 
+$(".nivel").closest("h2").hide();
+$(".record").closest("h2").hide();
+$(".wrap").hide();
 
 record = 0;
-jQuery(".boton").on('click', () => {
-        jQuery(".main").removeClass("main")
-        jQuery(".record").addClass("main")
-        jQuery(".boton").hide();
-        hacerJugada();
-        contador = 0;
-        jQuery(".contador").html(`<p>Nivel ${contador}</p>`)
+$(".boton").on('click', () => {
+  
+        nivel = 1;
+        time = 0;
+        grid = 2;
+        $(".record").closest("h2").hide();
+        $(".wrap").hide();
+        $(".nivel").html(`Nivel ${nivel}`)
+        
+        $(".menu").fadeOut(500, function(){
+            $(".menu").attr('style','display:none !important')
+            $(".nivel").closest("h2").show();
+            $(".wrap").show();
+            hacerJugada()
+            $(".progress-bar").css('width',`${time}%`).html(`${time/20}`);
+            intervalo = setInterval(tiempo, 1000);
+            
+        });
+       
 })
-
 function generarNumero(numero){
 	return (Math.random()*numero).toFixed(0);
 }
@@ -20,47 +34,85 @@ function colorRGB(){
 }
 
 
-function generarArray(contador=2){
+function generarArray(){
     var color = colorRGB();
-     arr = [];
-    for(let i=0; i<(contador+3); i++){
-        arr.push(color +");");
+    arr = [];
+    switch (nivel) {
+        case 50: grid=3; break;
+        case 120: grid=4; break;
+    }
+    for(let i=0; i<grid; i++){
+        arr.push([]);
+        for (let j = 0; j <grid; j++) {
+            arr[i].push(color +");");
+        }
     }
 
+  
+
     color = color+","+(Math.random().toFixed(2)*(0.95-0.30)+0.30)+");";
-    aleatorio = Math.floor(Math.random()*arr.length);
-    arr[aleatorio] = color
- 
+    aleatorioY = Math.floor(Math.random()*arr[0].length);
+    aleatorioX = Math.floor(Math.random()*arr.length);
+    arr[aleatorioY][aleatorioX] = color
+    
     return arr;
 }
 
-function hacerJugada(contador){
-    var arr = generarArray(contador); 
-    jQuery("#game").html("");
-    arr.map((item,index) => {
-        jQuery("#game").append(`<div id="${item}" class="mt-2" style="background-color:${item}"></div>`)
-    });
+function hacerJugada(){
+    var arr = generarArray(); 
+    $("#game").html("");
+    for(let i=0; i<arr.length; i++){
+        $("#game").append(`<div id='${i}' class="container d-flex justify-content-center mb-2 "></div>`);
+        
+        for (let j = 0; j <arr[i].length; j++) {
+          
+           $(`#${i}`).append(`<div id=${arr[i][j]} class="col-xs-2 m-2" style="background-color:${arr[i][j]});"></div>`);
+        }
+    }
    
 }
 
-jQuery(document).on('click','#game > div',function(e){
-   var id = jQuery(this).attr("id");
-   
+function tiempo(){
+    time+=20;
+    if(time<=100){
+        $(".progress-bar").css('width',`${time}%`).html(`${time/20}`);
+        console.log(time)
+    }else{
+        clearInterval(intervalo);
+        gameOver();
+    }
   
-   if(arr[aleatorio] == id){
-      
-    contador++;
-    jQuery(".contador").html(`<p>Nivel ${contador}</p>`)
-    hacerJugada(contador);
-   }else{
-       if(contador> record){
-            record = contador;
-       }
-      
-        jQuery(".boton").show();
-        jQuery("#main").addClass("main")
-        jQuery(".record").html(`<p>Record Anterior: ${record}</p>`).show();
+}
+function gameOver(){
+    if(nivel> record){
+        record = nivel;
+   }
+   if(record!=1){
+        $(".record").closest("h2").show();
+        $(".record").html(`Record Anterior: ${record}`).show();
+   }
+ clearInterval(intervalo);
+   $(".wrap").hide();
+   $(".menu").attr('style','display:"" !important')
+   $(".nivel").closest("h2").hide();
+}
+function reiniciarIntervalo(){
+    time = 0;
+    clearInterval(intervalo);
+    $(".progress-bar").css('width',`${time}%`).html(`${time/20}`);
+    intervalo = setInterval(tiempo, 1000);
+}
 
+$(document).on('click','#game > div div',function(e){
+   var id = $(this).attr("id");
+   
+   if(arr[aleatorioY][aleatorioX] == id){
+        reiniciarIntervalo();
+        nivel++;
+        $(".nivel").html(`Nivel ${nivel}`)
+        hacerJugada(nivel);
+   }else{
+        gameOver();
    }
    
 });
